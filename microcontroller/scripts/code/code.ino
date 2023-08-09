a395cdc15f2ae27824dd8e068f9c6bf09c337e59
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-08-09 15:02:22 -0600

    Code modularized

--- microcontroller/scripts/code/code.ino
+++ microcontroller/scripts/code/code.ino
@@ -2,27 +2,55 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-// MODES //
-#define AP_MODE 0
-#define CLIENT_MODE 1
+enum Modes {
+  PAIRING_MODE,
+  WORKING_MODE,
+  PENDING_RESTART
+};
 
-bool pendingRestart = false;
-int currentMode;
-// RESET BUTTON SETTINGS //
-const int ledPin = LED_BUILTIN;
 const int resetButtonPin = D3;
-unsigned long buttonPressStartTime = 0;
 const unsigned long resetDuration = 5000;
-// CLIENT WIFI SETTINGS //
+unsigned long buttonPressStartTime = 0;
+
+const int EEPROM_SIZE = 96;
 const int SSID_EEPROM_ADDR = 0;
-const int PASSWORD_EEPROM_ADDR = 32;
-const int MAX_ATTEMPTS = 5;
-// AP WIFI SETTINGS //
+const int PASS_EEPROM_ADDR = 32;
+
 const char* AP_SSID = "ISM-XXXXXXXX";
 const char* AP_PASS = "Garden123";
-// HTTP SERVER //
-ESP8266WebServer server(80);
 
+ESP8266WebServer server(80);
+Modes currentMode;
+
+void saveCredentialsToEEPROM(const String& ssid, const String& password);
+void readCredentialsFromEEPROM(String& ssid, String& password);
+void handleWifiCredentials();
+void enableAPMode();
+void enableClientMode(const String& ssid, const String& password);
+void enableSerial();
+void setupPins();
+void setupWiFi();
+void resetFlash();
+void checkResetButtonState();
+
+void setup() {
+  enableSerial();
+  setupPins();
+  setupWiFi();
+}
+
+void loop() {
+  checkResetButtonState();
+
+  switch (currentMode) {
+    case PAIRING_MODE:
+      server.handleClient();
+      break;
+    case PENDING_RESTART:
+      ESP.restart();
+      break;
+  }
+}
 
 void saveCredentialsToEEPROM(const String& ssid, const String& password) {
   int ssidLength = ssid.length();
@@ -34,9 +62,9 @@ void saveCredentialsToEEPROM(const String& ssid, const String& password) {
   EEPROM.write(SSID_EEPROM_ADDR + ssidLength, '\0');
 
   for (int i = 0; i < passLength; ++i) {
-    EEPROM.write(PASSWORD_EEPROM_ADDR + i, password[i]);
+    EEPROM.write(PASS_EEPROM_ADDR + i, password[i]);
   }
-  EEPROM.write(PASSWORD_EEPROM_ADDR + passLength, '\0');
+  EEPROM.write(PASS_EEPROM_ADDR + passLength, '\0');
 
   EEPROM.commit();
 }
@@ -54,22 +82,15 @@ void readCredentialsFromEEPROM(String& ssid, String& password) {
     }
   }
   ssid = String(eepromData);
-  Serial.print("SSID: ");
-  Serial.print(ssidLength);
-  Serial.print(ssid);
-
 
   for (int i = 0; i < 64; ++i) {
-    eepromData[i] = EEPROM.read(PASSWORD_EEPROM_ADDR + i);
+    eepromData[i] = EEPROM.read(PASS_EEPROM_ADDR + i);
     if (eepromData[i] == '\0') {
       passLength = i;
       break;
     }
   }
   password = String(eepromData);
-  Serial.print("APSS: ");
-  Serial.print(passLength);
-  Serial.print(password);
 }
 
 
@@ -77,26 +98,20 @@ void handleWifiCredentials() {
   String ssid = server.arg("ssid");
   String password = server.arg("password");
 
-  Serial.print("Received SSID: ");
-  Serial.println(ssid);
-  Serial.print("Received Password: ");
-  Serial.println(password);
-
   saveCredentialsToEEPROM(ssid, password);
-  
-  pendingRestart = true;
+
+  currentMode = PENDING_RESTART;
 
   server.send(200, "text/plain", "Wi-Fi credentials updated");
 }
 
 void enableAPMode() {
   Serial.println("Starting as Access Point (AP)...");
-  currentMode = AP_MODE;
+  currentMode = PAIRING_MODE;
 
   WiFi.mode(WIFI_AP);
   WiFi.softAP(AP_SSID, AP_PASS);
 
-  Serial.println("AP mode started.");
   Serial.print("> IP address: ");
   Serial.println(WiFi.softAPIP());
 
@@ -105,7 +120,7 @@ void enableAPMode() {
 }
 
 void enableClientMode(const String& ssid, const String& password) {
-  currentMode = CLIENT_MODE;
+  currentMode = WORKING_MODE;
   Serial.println("Starting as Client...");
 
   Serial.print("Connecting to ");
@@ -118,32 +133,27 @@ void enableClientMode(const String& ssid, const String& password) {
     Serial.print(".");
     attempts += 1;
 
-    if (attempts > MAX_ATTEMPTS) {
+    if (attempts > 5) {
       break;
     }
   }
-
-  Serial.println("\nConnected to Wi-Fi!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void enableSerial() {
   Serial.begin(115200);
-  delay(100);
   while (!Serial) {}
 }
 
 void setupPins() {
   pinMode(resetButtonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void setupWiFi() {
   String ssid, password;
 
-  EEPROM.begin(96);
+  EEPROM.begin(EEPROM_SIZE);
 
   readCredentialsFromEEPROM(ssid, password);
 
@@ -154,46 +164,25 @@ void setupWiFi() {
   }
 }
 
-void setup() {
-  enableSerial();
-  setupPins();
-  setupWiFi();
-}
-
-void erraseFlashMemory() {
-  Serial.println("Clearing flash memory...");
-
-  for (int i = 0; i < 96; i++) {
+void resetFlash() {
+  for (int i = 0; i < EEPROM_SIZE; i++) {
     EEPROM.write(i, 0);
   }
 
   EEPROM.commit();
-
-  Serial.println("Flash memory cleared.");
 }
 
 void checkResetButtonState() {
   if (digitalRead(resetButtonPin) == HIGH) {
     buttonPressStartTime = millis();
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
   } else {
     unsigned long buttonPressDuration = millis() - buttonPressStartTime;
-    digitalWrite(ledPin, LOW);
     if (buttonPressDuration >= resetDuration) {
+      digitalWrite(LED_BUILTIN, LOW);
+      currentMode = PENDING_RESTART;
+      resetFlash();
       delay(500);
-      erraseFlashMemory();
-      pendingRestart = true;
     }
-  }
-}
-void loop() {
-  checkResetButtonState();
-
-  if (currentMode == AP_MODE) {
-    server.handleClient();
-  }
-
-  if (pendingRestart) {
-    ESP.restart();
   }
 }
