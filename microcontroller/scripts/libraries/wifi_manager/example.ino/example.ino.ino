3577a6f166f0d8f4db8fe512a4f4a0c290b45435
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-08-10 23:56:00 -0600

    Reset button class added

--- microcontroller/scripts/libraries/wifi_manager/example.ino/example.ino.ino
+++ microcontroller/scripts/libraries/wifi_manager/example.ino/example.ino.ino
@@ -4,6 +4,7 @@
 #include <ESP8266WebServer.h>
 
 #define ledPin LED_BUILTIN
+#define resetButtonPin D3
 
 enum WiFiModes {
   CONNECTION_MODE,
@@ -187,16 +188,78 @@ WiFiModes CustomWiFiManager::getCurrentMode() {
   return this->currentMode;
 }
 
-CustomWiFiManager wifiManager;
+class ResetButton {
+public:
+  typedef void (*ResetCallback)();
 
+  ResetButton();
+
+  void begin(uint8_t buttonPin, unsigned long resetDuration);
+  void setResetCallback(ResetCallback callback);
+  void checkStatus();
+
+private:
+  int buttonPin;
+  unsigned long resetDuration;
+  unsigned long lastPressTime;
+  bool resetTriggered;
+  ResetCallback resetCallback;
+};
+
+ResetButton::ResetButton()
+  : lastPressTime(0), resetTriggered(false), resetCallback(nullptr) {}
+
+void ResetButton::begin(uint8_t buttonPin, unsigned long resetDuration = 5000) {
+  this->buttonPin = buttonPin;
+  this->resetDuration = resetDuration;
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void ResetButton::setResetCallback(ResetCallback callback) {
+  resetCallback = callback;
+}
+
+
+void ResetButton::checkStatus() {
+  if (digitalRead(buttonPin) == LOW) {
+    if (!resetTriggered) {
+      lastPressTime = millis();
+      resetTriggered = true;
+    } else if (millis() - lastPressTime >= resetDuration) {
+      if (resetCallback) {
+        resetCallback();
+      }
+      resetTriggered = false;
+    }
+  } else {
+    resetTriggered = false;
+  }
+}
+
+
+
+
+CustomWiFiManager wifiManager;
+ResetButton resetButton;
+
+
+void resetCallback() {
+  Serial.println("RESET CALLBACK TRIGGERED...");
+  delay(3000);
+  wifiManager.forgetCredentials();
+  ESP.restart();
+}
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
   wifiManager.begin();
+  resetButton.begin(resetButtonPin, 3000);
+  resetButton.setResetCallback(resetCallback);
 }
 
 void loop() {
+
   WiFiModes mode = wifiManager.getCurrentMode();
 
   switch (mode) {
@@ -211,12 +274,11 @@ void loop() {
       break;
     case PENDING_RESTART:
       Serial.println("Restarting...");
-      delay(1000);
       ESP.restart();
       break;
   }
   delay(500);
-
+  resetButton.checkStatus();
   Serial.print("Mode: ");
   Serial.println(mode);
 }
